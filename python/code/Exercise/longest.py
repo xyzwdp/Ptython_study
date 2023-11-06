44c7e95d3193e676b8eb9733c1c9f67aa60c0796
WDP <xyzwdp@163.com>
2023-11-06 15:30:28 +0800

    提交新增代码

--- python/code/Exercise/longest.py
+++ python/code/Exercise/longest.py
@@ -1,1 +1,24 @@
 # _*_ coding:UTF-8 _*_
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # if not s:return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:max_len = cur_len
+            lookup.add(s[i])
+            print(lookup)
+        return max_len
+
+if __name__ == '__main__':
+    A=Solution()
+    s = "abcabcbb"
+    print(A.lengthOfLongestSubstring(s))
--- python/code/Exercise/twoSum.py
+++ python/code/Exercise/twoSum.py
@@ -1,1 +1,17 @@
 # _*_ coding:UTF-8 _*_
+class Sum:
+    def twosum(self,num1,num2):
+        """
+
+        :param num1: list[int]
+        :param num2: list[int]
+        :return: list[int]
+        """
+        #设置一个空列表
+        result=[]
+        # 判断循环取哪个
+        if len(num1)>len(num2):
+            for i in range(len(num1)):
+                sum=num1.index[i]+num2.index[i]
+        else:
+            pass
